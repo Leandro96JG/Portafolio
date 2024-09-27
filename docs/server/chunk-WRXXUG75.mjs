cea318e75e3663d2a79578d67ea8b2835813b487
Leandro J. Gonzalez <Jesuusgonzalees@gmail.com>
2024-09-27 15:45:41 -0300

    ng build docs

---
+++ docs/server/chunk-WRXXUG75.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as n,b as i}from"./chunk-HITXCD2X.mjs";import"./chunk-W3F4DPXE.mjs";import{Wa as e,h as r,n as t,pa as m}from"./chunk-4R4MW575.mjs";import"./chunk-5XUXGTUW.mjs";var w=(()=>{class o{static \u0275fac=function(p){return new(p||o)};static \u0275mod=t({type:o});static \u0275inj=r({imports:[m,i,e,n]})}return o})();export{w as DashboardModule};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-LI5H4PBM.mjs";import"./chunk-TYKL2RCG.mjs";import"./chunk-HITXCD2X.mjs";import"./chunk-W3F4DPXE.mjs";import"./chunk-4R4MW575.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
